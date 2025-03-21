be3a12d691123346ca02225b59c9da0cdf136cd7
Nevskiy590 <kotomanov.1990@mail.ru>
2025-03-21 14:16:13 +1000

    Add files via upload

---
+++ ЛР_ОПИ.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main() {
+	int n;
+	std::cout << "Размер массива: ";
+	std::cin >> n;
+	int* mas = new int[n] {0};
+	for (int i = 0; i < n; i++) {
+		mas[i] = i*i ;
+		std::cout << "massive[" << i << "] = " << mas[i] << std::endl;
+	}
+}
